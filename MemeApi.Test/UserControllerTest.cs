0da930cd4935d1d305f7ffcb33d51ca2654fe976
oeje <oeje@itu.dk>
2021-08-29 03:32:30 +0200

    First commit

---
+++ MemeApi.Test/UserControllerTest.cs
@@ -0,0 +1,15 @@
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace MemeApi.Test
+{
+    public class UserControllerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ MemeApi/Models/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeApi.Models
+{
+    public class UserDTO
+    {
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string password { get; set; }
+        public string ProfilePicFile { get; set; }
+    }
+}
