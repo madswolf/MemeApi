8d7b129281878b0ab3e9d757284128f65554c0e0
oeje <MadsWJespersen@users.noreply.github.com>
2021-08-29 03:32:30 +0200

    First commit

---
+++ MemeApi/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeApi.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string ProfilePicFile { get; set; }
+        public string passwordHash { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
