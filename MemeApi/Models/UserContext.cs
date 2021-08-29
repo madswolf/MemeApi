982b9a33581beee0745f708744cea463a5e2620a
Madswolf <github@madswolf.dk>
2021-08-29 03:32:30 +0200

    First commit

---
+++ MemeApi/Models/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MemeApi.Models
+{
+    public class UserContext : DbContext
+    {
+
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
