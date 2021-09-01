5b66ebc099bdc1f7883423df3303508674f92d03
Madswolf <github@madswolf.dk>
2021-09-01 02:29:42 +0200

    Added support for authorization for users

--- MemeApi.Test/UserControllerTest.cs
+++ MemeApi.Test/UserControllerTest.cs
@@ -1,14 +1,47 @@
 using FluentAssertions;
 using System;
 using Xunit;
+using Microsoft.EntityFrameworkCore;
+using MemeApi.Models.Context;
+using MemeApi.Controllers;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace MemeApi.Test
 {
     public class UserControllerTest
     {
+        private readonly DbContextOptions<MemeContext> ContextOptions = new DbContextOptionsBuilder<MemeContext>().UseInMemoryDatabase("MemeList").Options;
+
         [Fact]
-        public void Test1()
+        public async Task Test1Async()
         {
+            using (var context = new MemeContext(ContextOptions))
+            {
+                var controller = new UsersController(context, new ConfigurationBuilder().AddJsonFile("appsettings.json").Build());
+
+                // given
+
+                var userDTO = new Models.UserCreationDTO
+                {
+                    Username = "Test",
+                    Email = "Test",
+                    password = "Test"
+                };
+
+                await controller.PostUser(userDTO);
+
+                (await context.Users.CountAsync()).Should().Be(1);
+                var user = await context.Users.FirstAsync();
+
+                user.Username.Should().Be(userDTO.Username);
+                user.Email.Should().Be(userDTO.Email);
+                user.PasswordHash.Should().NotBe(userDTO.password);
+
+                // Then
+
+            }
             Assert.True(true);
         }
     }
--- MemeApi/Startup.cs
+++ MemeApi/Startup.cs
@@ -1,11 +1,14 @@
 using MemeApi.Models;
 using MemeApi.Models.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace MemeApi
 {
@@ -30,6 +33,21 @@ namespace MemeApi
             services.AddControllers().AddNewtonsoftJson(options =>
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
             );
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                });
+            
             //services.AddSwaggerGen(c =>
             //{
             //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MemeApi", Version = "v1" });
@@ -46,12 +64,14 @@ namespace MemeApi
                 //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MemeApi v1"));
             }
 
+            app.UseAuthentication();
+
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
