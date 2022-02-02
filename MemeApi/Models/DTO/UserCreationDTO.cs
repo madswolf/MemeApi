af43a5c14e1d58dc24f2ee8b79c4de1bb083cdef
Madswolf <github@madswolf.dk>
2022-02-02 23:56:48 +0100

    UserController tests

--- MemeApi/Models/DTO/UserCreationDTO.cs
+++ MemeApi/Models/DTO/UserCreationDTO.cs
@@ -9,6 +9,6 @@ namespace MemeApi.Models
         [Required]
         public string Email { get; set; }
         [Required]
-        public string password { get; set; }
+        public string Password { get; set; }
     }
 }
