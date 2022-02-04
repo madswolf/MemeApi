1e125a182086db9906d95cec797a2b4fad853197
oeje <oeje@itu.dk>
2022-02-05 00:34:09 +0100

    Refactored MemeBottomtext and MemeTopText into a single class with and enum along with tests

--- MemeApi/Controllers/MemeTextsController.cs
+++ MemeApi/Controllers/MemeTextsController.cs
@@ -5,52 +5,58 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MemeApi.Models.Entity;
 
 namespace MemeApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class MemeBottomTextsController : ControllerBase
+    public class MemeTextsController : ControllerBase
     {
         private readonly MemeContext _context;
 
-        public MemeBottomTextsController(MemeContext context)
+        public MemeTextsController(MemeContext context)
         {
             _context = context;
         }
 
         // GET: api/MemeBottomTexts
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemeBottomText>>> GetBottomTexts()
+        public async Task<ActionResult<IEnumerable<MemeText>>> GetBottomTexts()
         {
-            return await _context.BottomTexts.ToListAsync();
+            return await _context.Texts.ToListAsync();
         }
 
         // GET: api/MemeBottomTexts/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<MemeBottomText>> GetMemeBottomText(long id)
+        public async Task<ActionResult<MemeText>> GetMemeBottomText(long id)
         {
-            var memeBottomText = await _context.BottomTexts.FindAsync(id);
+            var memeBottomText = await _context.Texts.FindAsync(id);
 
             if (memeBottomText == null)
             {
                 return NotFound();
             }
 
-            return memeBottomText;
+            return Ok(memeBottomText);
         }
 
         // PUT: api/MemeBottomTexts/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutMemeBottomText(long id, MemeBottomText memeBottomText)
+        public async Task<IActionResult> UpdateMemeText(long id, string newMemeBottomText, MemeTextPosition? newMemeTextPosition = null)
         {
-            if (id != memeBottomText.Id)
-            {
-                return BadRequest();
-            }
+            var memeText = await _context.Texts.FindAsync(id);
 
-            _context.Entry(memeBottomText).State = EntityState.Modified;
+            if (memeText == null)
+            {
+                return NotFound();
+            }
+            memeText.Memetext = newMemeBottomText;
+            if (newMemeTextPosition != null)
+            {
+                memeText.postion = (MemeTextPosition)newMemeTextPosition;
+            }
 
             try
             {
@@ -74,25 +80,28 @@ namespace MemeApi.Controllers
         // POST: api/MemeBottomTexts
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<MemeBottomText>> PostMemeBottomText(MemeBottomText memeBottomText)
+        public async Task<ActionResult<MemeText>> CreateMemeBottomText(string text, MemeTextPosition position)
         {
-            _context.BottomTexts.Add(memeBottomText);
+            var memeText = new MemeText();
+            memeText.Memetext = text;
+            memeText.postion = position;
+            _context.Texts.Add(memeText);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetMemeBottomText", new { id = memeBottomText.Id }, memeBottomText);
+            return CreatedAtAction("CreateMemeBottomText", new { id = memeText.Id }, memeText);
         }
 
         // DELETE: api/MemeBottomTexts/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteMemeBottomText(long id)
         {
-            var memeBottomText = await _context.BottomTexts.FindAsync(id);
+            var memeBottomText = await _context.Texts.FindAsync(id);
             if (memeBottomText == null)
             {
                 return NotFound();
             }
 
-            _context.BottomTexts.Remove(memeBottomText);
+            _context.Texts.Remove(memeBottomText);
             await _context.SaveChangesAsync();
 
             return NoContent();
@@ -100,7 +109,7 @@ namespace MemeApi.Controllers
 
         private bool MemeBottomTextExists(long id)
         {
-            return _context.BottomTexts.Any(e => e.Id == id);
+            return _context.Texts.Any(e => e.Id == id);
         }
     }
 }
--- MemeApi/Models/Entity/Meme.cs
+++ MemeApi/Models/Entity/Meme.cs
@@ -1,10 +1,11 @@
-﻿namespace MemeApi.Models.Entity
+﻿using System.Collections.Generic;
+
+namespace MemeApi.Models.Entity
 {
     public class Meme : Votable
     {
         public MemeVisual MemeVisual { get; set; }
         public MemeSound MemeSound { get; set; }
-        public MemeTopText MemeTopText { get; set; }
-        public MemeBottomText MemeBottomtext { get; set; }
+        public List<MemeText> MemeTexts { get; set; }
     }
 }
