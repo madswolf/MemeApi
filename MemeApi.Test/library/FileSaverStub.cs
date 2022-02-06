564bed3f408adec707dd53d450caed382c4ec37c
Madswolf <github@madswolf.dk>
2022-02-06 01:34:23 +0100

    Added file saving/removing logic to MemeVisualsController and add tests for memevisualscontroller

---
+++ MemeApi.Test/library/FileSaverStub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MemeApi.library;
+using Microsoft.AspNetCore.Http;
+
+namespace MemeApi.Test.library
+{
+    internal class FileSaverStub : IFileSaver
+    {
+        public void SaveFile(IFormFile file, string path)
+        {
+            return;
+        }
+    }
+}
---
+++ MemeApi/library/FileRemover.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace MemeApi.library
+{
+    public class FileRemover : IFileRemover
+    {
+        public void RemoveFile(string path)
+        {
+            File.Delete(path);
+        }
+    }
+}
