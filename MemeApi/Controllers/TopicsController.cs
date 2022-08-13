7a8c4f1bdfd5fbef2577c6014b2745144b0ca155
oeje <Mads_1997@live.dk>
2022-08-13 22:26:02 +0200

    topics

--- MemeApi/Controllers/TopicsController.cs
+++ MemeApi/Controllers/TopicsController.cs
@@ -1,10 +1,14 @@
-﻿using MemeApi.Models.Context;
+﻿using System;
+using MemeApi.Models.Context;
 using MemeApi.Models.Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using MemeApi.library.repositories;
+using MemeApi.Models.DTO;
 
 namespace MemeApi.Controllers
 {
@@ -13,10 +17,11 @@ namespace MemeApi.Controllers
     public class TopicsController : ControllerBase
     {
         private readonly MemeContext _context;
-
-        public TopicsController(MemeContext context)
+        private readonly UserRepository _userRepository;
+        public TopicsController(MemeContext context, UserRepository userRepository)
         {
             _context = context;
+            _userRepository = userRepository;
         }
 
         // GET: api/Topics
@@ -28,7 +33,7 @@ namespace MemeApi.Controllers
 
         // GET: api/Topics/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Topic>> GetTopic(long id)
+        public async Task<ActionResult<Topic>> GetTopic(int id)
         {
             var topic = await _context.Topics.FindAsync(id);
 
@@ -43,14 +48,24 @@ namespace MemeApi.Controllers
         // PUT: api/Topics/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutTopic(long id, Topic topic)
+        [Route("[controller]/{topicId}/mod/{userId}")]
+        public async Task<IActionResult> ModUser(int topicId, int userId)
         {
-            if (id != topic.Id)
+            var topic = await _context.Topics.FindAsync(topicId);
+            var ownerId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (topic == null || ownerId == null)
             {
-                return BadRequest();
+                return NotFound();
             }
 
-            _context.Entry(topic).State = EntityState.Modified;
+            if (topic.Owner.Id != int.Parse(ownerId)) return Unauthorized();
+
+            var userToMod = await _context.Users.FindAsync(userId);
+
+            if(userToMod == null) return NotFound();
+
+            topic.Moderators.Add(userToMod);
 
             try
             {
@@ -58,7 +73,7 @@ namespace MemeApi.Controllers
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!TopicExists(id))
+                if (!TopicExists(topicId))
                 {
                     return NotFound();
                 }
@@ -68,15 +83,28 @@ namespace MemeApi.Controllers
                 }
             }
 
-            return NoContent();
+            return Ok();
         }
 
         // POST: api/Topics
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Topic>> CreateTopic(Topic topic)
+        public async Task<ActionResult<Topic>> CreateTopic(TopicCreationDTO topicCreationDTO)
         {
-            _context.Topics.Add(topic);
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null) return Unauthorized();
+            var user = await _userRepository.GetUser(int.Parse(userId));
+
+            var topic = new Topic()
+            {
+                Owner = user,
+                Name = topicCreationDTO.TopicName,
+                Description = topicCreationDTO.Description,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+             _context.Topics.Add(topic);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetTopic", new { id = topic.Id }, topic);
@@ -84,18 +112,37 @@ namespace MemeApi.Controllers
 
         // DELETE: api/Topics/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTopic(long id, User user)
+        public async Task<IActionResult> DeleteTopic(int id)
         {
             var topic = await _context.Topics.FindAsync(id);
-            if (topic == null)
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (topic == null || userId == null)
             {
                 return NotFound();
             }
+
+            if (topic.Owner.Id != int.Parse(userId)) return Unauthorized();
 
             _context.Topics.Remove(topic);
             await _context.SaveChangesAsync();
 
             return NoContent();
+        }
+
+        [HttpPost]
+        [Route("[controller]/{id}")]
+        public async Task<IActionResult> DeleteVotable(int id)
+        {
+            var votable = await _context.Votables.FindAsync(id);
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (votable == null || userId == null) return NotFound();
+
+            if (votable.Topic.Owner.Id != int.Parse(userId) || votable.Topic.Moderators.All(x => x.Id != int.Parse(userId)))
+                return Unauthorized();
+
+            _context.Votables.Remove(votable);
+            return Ok();
         }
 
         private bool TopicExists(long id)
--- MemeApi/Models/Entity/Votable.cs
+++ MemeApi/Models/Entity/Votable.cs
@@ -4,7 +4,7 @@ namespace MemeApi.Models.Entity
 {
     public class Votable
     {
-        public long Id { get; set; }
+        public int Id { get; set; }
         public Topic Topic { get; set; }
         public List<Vote> Votes { get; set; }
     }
