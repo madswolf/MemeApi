ef7157e75d8f790ff1f5fb1f488c2afbf6165ef7
oeje <Mads_1997@live.dk>
2022-08-18 00:10:47 +0200

    added update user

--- MemeApi/library/FileSaver.cs
+++ MemeApi/library/FileSaver.cs
@@ -1,13 +1,20 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace MemeApi.library
 {
     public class FileSaver : IFileSaver
     {
+        private readonly IConfiguration _configuration;
+        public FileSaver(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public async void SaveFile(IFormFile file, string path)
         {
-            await using Stream fileStream = new FileStream(Path.Combine(path, file.FileName), FileMode.Create);
+            await using Stream fileStream = new FileStream(Path.Combine(_configuration["BaseUploadFolder"], path, file.FileName), FileMode.Create);
             await file.CopyToAsync(fileStream);
         }
     }
