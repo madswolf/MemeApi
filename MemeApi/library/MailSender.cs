b7cf3159faa5e5d7d9c2bfdfdc89785858342070
Madswolf <github@madswolf.dk>
2022-08-19 00:22:54 +0200

    added preliminary mail sending

---
+++ MemeApi/library/MailSender.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace MemeApi.library
+{
+    public class MailSender : IMailSender
+    {
+        public Task<bool> sendMail(MailAddress recipient, string subject, string body)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
