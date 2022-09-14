d44f4499c30696495edcc7d8a07105e0f0d69b45
oeje <MadsWJespersen@users.noreply.github.com>
2022-09-14 02:52:33 +0200

    Fixed minor frontend integration issue and removed unused package and updated packages

--- MemeApi/Controllers/SoundsController.cs
+++ MemeApi/Controllers/SoundsController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace MemeApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class SoundsController : ControllerBase
     {
