b03c6db7ea9bbc0ac4e15501b242c3c15e3497af
oeje <43292936+MadsWJespersen@users.noreply.github.com>
2023-03-31 03:24:23 +0200

    Made memes and components have default topics

---
+++ MemeApi/Models/DTO/TextDTO.cs
@@ -0,0 +1,24 @@
+﻿using MemeApi.Models.Entity;
+using System.Collections.Generic;
+
+namespace MemeApi.Models.DTO
+{
+    /// <summary>
+    /// A DTO for textual meme components
+    /// </summary>
+    public class TextDTO
+    {
+        /// <summary>
+        /// Actual content of the text
+        /// </summary>
+        public string Text { get; set; }
+        /// <summary>
+        /// Position of the textual component
+        /// </summary>
+        public MemeTextPosition Position { get; set; }
+        /// <summary>
+        /// Topics that the text belongs to
+        /// </summary>
+        public List<string> Topics { get; set; }
+    }
+}
