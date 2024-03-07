5a76c615a3adeb3468769cf2f6a8b04123e3a160
Madswolf <github@madswolf.dk>
2024-03-07 22:44:17 +0100

    fix dumb

--- MemeApi.Test/library/FileRemoverStub.cs
+++ MemeApi.Test/library/FileRemoverStub.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using MemeApi.library.Services.Files;
 using System.Threading.Tasks;
-using MemeApi.library.Services.Files;
 
 namespace MemeApi.Test.library
 {
     internal class FileRemoverStub : IFileRemover
     {
-        public void RemoveFile(string path)
+        public Task RemoveFile(string path)
         {
-            return;
+            return Task.CompletedTask;
         }
     }
 }
