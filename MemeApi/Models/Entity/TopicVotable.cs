f7ba068d19f2a3f9ed6501c50209f54a4d8b8771
Madswolf <mads@madswolf.dk>
2024-08-11 01:54:38 +0200

    Made Explicit classes work for join tables

---
+++ MemeApi/Models/Entity/TopicVotable.cs
@@ -0,0 +1,12 @@
+﻿#nullable disable warnings
+namespace MemeApi.Models.Entity;
+
+public class TopicVotable
+{
+    public string VotableId { get; set; }
+    public Votable Votable { get; set; }
+
+    public string TopicID { get; set; }
+    public Topic Topic { get; set; }
+
+}
