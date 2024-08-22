e50cd7e9a59e194ceb60d3a72b60fdc4db04a384
Madswolf <github@madswolf.dk>
2024-08-23 01:28:35 +0200

    Added middleware for external user ids to unify user claims

--- MemeApi/MIddleware/SwaggerAuthenticationMiddleware.cs
+++ MemeApi/MIddleware/SwaggerAuthenticationMiddleware.cs
@@ -6,7 +6,7 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MemeApi;
+namespace MemeApi.MIddleware;
 public class SwaggerAuthenticationMiddleware
 {
 
