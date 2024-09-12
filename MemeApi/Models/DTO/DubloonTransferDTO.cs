90ad0bf41847ba443434bb6aa5cfbc8a5a768888
Madswolf <mads@madswolf.dk>
2024-09-13 01:08:35 +0200

    transaction

---
+++ MemeApi/Models/DTO/DubloonTransferDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MemeApi.Models.DTO;
+/// <summary>
+/// A DTO of user info
+/// </summary>
+/// <param name="OtherUserId"> The other party user</param>
+/// <param name="DubloonsToTransfer"> The number of dubloons to transfer to the other user </param>
+public class DubloonTransferDTO
+{
+    public string OtherUserId { get; set; }
+    public int DubloonsToTransfer { get; set; }
+}
---
+++ MemeApi/Models/Entity/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using MemeApi.Models.DTO;
+using System;
+
+namespace MemeApi.Models.Entity;
+
+public class Transaction : DubloonEvent
+{
+    public User Other {  get; set; }
+    public override DubloonEventInfoDTO ToDubloonEventInfoDTO() => new TransactionInfoDTO(
+            Id,
+            Owner.UserName,
+            (int)Math.Floor(Dubloons),
+            Other.UserName
+        );
+}
