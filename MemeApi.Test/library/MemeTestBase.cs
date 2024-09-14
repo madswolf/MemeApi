bc1fcd91154ef95ca7b0a635c85016e2cea8d7ff
Madswolf <github@madswolf.dk>
2024-09-14 02:02:31 +0200

    in the middle of fixing user tests

--- MemeApi.Test/library/MemeTestBase.cs
+++ MemeApi.Test/library/MemeTestBase.cs
@@ -1,8 +1,10 @@
-﻿using MemeApi.library;
+﻿using MemeApi.Controllers;
+using MemeApi.library;
 using MemeApi.library.repositories;
 using MemeApi.library.Services.Files;
 using MemeApi.Models.Context;
 using MemeApi.Models.Entity;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
@@ -11,6 +13,8 @@ using Microsoft.Extensions.Options;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -28,6 +32,8 @@ public class MemeTestBase : IAsyncLifetime
     protected VotableRepository _votableRepository;
     protected MemeApiSettings _settings;
     protected IntegrationTestFactory _fixture;
+    protected UserManager<User> _userManager;
+    protected SignInManager<User> _signInManager;
 
     protected MemeRenderingService _memeRenderingService;
 
@@ -56,7 +62,10 @@ public class MemeTestBase : IAsyncLifetime
         _visualRepository = new VisualRepository(_context, new FileSaverStub(), new FileRemoverStub(), _topicRepository, _userRepository);
         _textRepository = new TextRepository(_context, _topicRepository, _userRepository);
         _memeRepository = new MemeRepository(_context, _visualRepository, _textRepository, _topicRepository, _settings, _userRepository);
-        _votableRepository = new VotableRepository(_context, _settings) ;
+        _votableRepository = new VotableRepository(_context, _settings);
+
+        _userManager = new FakeUserManager();
+        _signInManager = new FakeSignInManager();
     }
 
     public static HttpContext GetMockedHttpContext()
@@ -66,6 +75,24 @@ public class MemeTestBase : IAsyncLifetime
         context.RequestServices = new Mock<IServiceProvider>().Object;
         context.Session = mockSession.Object;
         return context;
+    }
+
+    protected static void SetUserNameIdentifier(UsersController controller, string userId)
+    {
+        var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId),
+            };
+        var identity = new ClaimsIdentity(claims, "Test");
+        var claimsPrincipal = new ClaimsPrincipal(identity);
+
+        var mockPrincipal = new Mock<IPrincipal>();
+        mockPrincipal.Setup(x => x.Identity).Returns(identity);
+        mockPrincipal.Setup(x => x.IsInRole(It.IsAny<string>())).Returns(true);
+
+        var mockHttpContext = new Mock<HttpContext>();
+        mockHttpContext.Setup(m => m.User).Returns(claimsPrincipal);
+        controller.ControllerContext.HttpContext = mockHttpContext.Object;
     }
     public static UserManager<TUser> TestUserManager<TUser>(IUserStore<TUser>? store = null) where TUser : class
     {
@@ -77,7 +104,7 @@ public class MemeTestBase : IAsyncLifetime
         var userValidators = new List<IUserValidator<TUser>>();
         var validator = new Mock<IUserValidator<TUser>>();
         userValidators.Add(validator.Object);
-        var pwdValidators = new List<PasswordValidator<TUser>>{new()};
+        var pwdValidators = new List<PasswordValidator<TUser>> { new() };
         var userManager = new UserManager<TUser>(store, options.Object, new PasswordHasher<TUser>(),
             userValidators, pwdValidators, new UpperInvariantLookupNormalizer(),
             new IdentityErrorDescriber(), new Mock<IServiceProvider>().Object,
@@ -85,6 +112,20 @@ public class MemeTestBase : IAsyncLifetime
         validator.Setup(v => v.ValidateAsync(userManager, It.IsAny<TUser>()))
             .Returns(Task.FromResult(IdentityResult.Success)).Verifiable();
         return userManager;
+    }
+
+    public static SignInManager<TUser> TestSignInManager<TUser>(UserManager<TUser> userManager) where TUser : class
+    {
+        var signInManager = new Mock<SignInManager<TUser>>(
+                    userManager,
+                    new HttpContextAccessor(),
+                    new Mock<IUserClaimsPrincipalFactory<TUser>>().Object,
+                    new Mock<IOptions<IdentityOptions>>().Object,
+                    new Mock<ILogger<SignInManager<TUser>>>().Object,
+                    new Mock<Microsoft.AspNetCore.Authentication.IAuthenticationSchemeProvider>().Object
+                  );
+
+        return signInManager.Object;
     }
 
     private async Task SeedDB()
@@ -122,7 +163,7 @@ public class MemeTestBase : IAsyncLifetime
             LastUpdatedAt = DateTime.UtcNow
         };
 #pragma warning restore CS8601 // Possible null reference assignment.
-        _context.Users.Add(admin);  
+        _context.Users.Add(admin);
         _context.Topics.Add(defaultTopic);
         _context.Topics.Add(memeOfTheDayTopic);
         await _context.SaveChangesAsync();
@@ -144,8 +185,51 @@ public class MemeTestBase : IAsyncLifetime
 
     public async Task DisposeAsync() => await _fixture.ResetDatabase();
 
-    public async Task InitializeAsync(){
+    public async Task InitializeAsync()
+    {
         await _fixture.ResetDatabase();
         await SeedDB();
+    }
+
+    public class FakeUserManager : UserManager<User>
+    {
+        public FakeUserManager()
+            : base(new Mock<IUserStore<User>>().Object,
+              new Mock<IOptions<IdentityOptions>>().Object,
+              new Mock<IPasswordHasher<User>>().Object,
+              new IUserValidator<User>[0],
+              new IPasswordValidator<User>[0],
+              new Mock<ILookupNormalizer>().Object,
+              new Mock<IdentityErrorDescriber>().Object,
+              new Mock<IServiceProvider>().Object,
+              new Mock<ILogger<UserManager<User>>>().Object)
+        { }
+
+        public override Task<IdentityResult> CreateAsync(User user, string password)
+        {
+            return Task.FromResult(IdentityResult.Success);
+        }
+
+        public override Task<IdentityResult> AddToRoleAsync(User user, string role)
+        {
+            return Task.FromResult(IdentityResult.Success);
+        }
+
+        public override Task<string> GenerateEmailConfirmationTokenAsync(User user)
+        {
+            return Task.FromResult(Guid.NewGuid().ToString());
+        }
+    }
+    public class FakeSignInManager : SignInManager<User>
+    {
+        public FakeSignInManager()
+                : base(new FakeUserManager(),
+                     new Mock<IHttpContextAccessor>().Object,
+                     new Mock<IUserClaimsPrincipalFactory<User>>().Object,
+                     new Mock<IOptions<IdentityOptions>>().Object,
+                     new Mock<ILogger<SignInManager<User>>>().Object,
+                     new Mock<IAuthenticationSchemeProvider>().Object,
+                     new Mock<IUserConfirmation<User>>().Object)
+        { }
     }
 }
