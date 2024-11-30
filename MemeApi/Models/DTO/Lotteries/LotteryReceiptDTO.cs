c7faecfd749694577bcb34d8774602d0c0bad734
Madswolf <github@madswolf.dk>
2024-11-30 03:05:37 +0100

    stuff

---
+++ MemeApi/Models/DTO/Lotteries/LotteryReceiptDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace MemeApi.Models.DTO.Lotteries;
+
+public record LotteryReceiptDTO
+{
+    public string LotteryId { get; init; }
+    public int TotalTicketCount { get; init; }
+    public int TotalDubloonsSpent { get; init; }
+    public List<LotteryItemReceiptDTO> Items { get; init; }
+}
+
+public record LotteryItemReceiptDTO
+{
+    public string ItemId { get; init; }
+    public string ItemName { get; init; }
+    public int ItemCount { get; init; }
+}
